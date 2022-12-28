387333aaff4e890bbfbff8386f6958e28cd95f63
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2022-12-28 20:43:37 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 15:07:23 2022
+
+@author: Karam Alhanatlh
+"""
+"""
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text = "Karam Suleiman ")
+    
+if __name__ =='__main__':
+    MyApp().run()
+
+
+"""
+import kivy
+#kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+class HelloWorld(App):
+    
+    def build(self):
+        return Label(text="Karam!!!!!")
+
+helloWorld = HelloWorld()
+
+helloWorld.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
